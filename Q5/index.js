05bcf045aed3f44018607209929f9590724a50ef
Qalb-e-Abbas <sqabbas1994@gmail.com>
2023-08-01 23:05:39 +0500

    Q5 is committed on GitHub

---
+++ Q5/index.js
@@ -0,0 +1,4 @@
+var famous_person = "Muhammad (S.A.W)";
+var qoute = "Wisdom is the lost property of a Muslim;\n hence collect it from whoever you find it with.";
+var message = "".concat(famous_person, " once said, \"").concat(qoute, "\"");
+console.log(message);
---
+++ Q5/index.ts
@@ -0,0 +1,8 @@
+let famous_person : string = "Muhammad (S.A.W)";
+
+let qoute : string = `Wisdom is the lost property of a Muslim;
+ hence collect it from whoever you find it with.`;
+
+ let message: string = `${famous_person} once said, "${qoute}"`;
+
+console.log(message);    
