0b55ba71e695b4041ef2644de219ef782521ec2b
Qalb-e-Abbas <sqabbas1994@gmail.com>
2023-08-01 23:58:35 +0500

    Q7 is committed on GitHub Repository

---
+++ Q7/index.js
@@ -0,0 +1,20 @@
+// Addition
+var num1 = 5;
+var num2 = 3;
+var addition = num1 + num2;
+console.log("Addition Result:", addition);
+// Subtraction 
+var num3 = 15;
+var num4 = 7;
+var subtraction = num3 - num4;
+console.log("Subtraction Result:", subtraction);
+// Multiplication 
+var num5 = 4;
+var num6 = 2;
+var multiplication = num5 * num6;
+console.log("Multiplication Result:", multiplication);
+//Division
+var num7 = 32;
+var num8 = 4;
+var division = num7 / num8;
+console.log("Division Result:", division);
---
+++ Q7/index.ts
@@ -0,0 +1,23 @@
+// Addition
+let num1 : number = 5;
+let num2 : number = 3;
+let addition = num1+num2;
+console.log("Addition Result:",addition);
+
+// Subtraction 
+let num3 : number = 15;
+let num4 : number = 7;
+let subtraction = num3-num4;
+console.log("Subtraction Result:",subtraction);
+
+// Multiplication 
+let num5 : number = 4;
+let num6 : number = 2;
+let multiplication = num5*num6;
+console.log("Multiplication Result:",multiplication);
+
+//Division
+let num7 : number = 32;
+let num8 : number = 4;
+let division = num7/num8;
+console.log("Division Result:",division);
