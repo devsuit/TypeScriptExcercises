b81b72c72b6bcda67becf973e94255bdfcdb94a8
Qalb-e-Abbas <sqabbas1994@gmail.com>
2023-08-01 23:42:07 +0500

    Q6 is committed on GitHub Repo

---
+++ Q6/index.js
@@ -0,0 +1,4 @@
+var personName = "\t  \n Qalbe Abbas \t  \n";
+console.log("Name with Whitespace:", personName);
+var stripedName = personName.trim();
+console.log("Striped Name:", stripedName);
---
+++ Q6/index.ts
@@ -0,0 +1,7 @@
+let personName :string = "\t  \n Qalbe Abbas \t  \n";
+
+console.log("Name with Whitespace:", personName);
+
+let stripedName : string = personName.trim();
+
+console.log("Striped Name:", stripedName);
