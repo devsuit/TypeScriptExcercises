8abca308b9e020b9c66d205601de8ce202b0b0f8
Qalb-e-Abbas <sqabbas1994@gmail.com>
2023-08-01 20:37:17 +0500

    Q4 is committed on GitHub

---
+++ Q4/index.js
@@ -0,0 +1,3 @@
+var Auth = "Muhammad (S.A.W)";
+var Qoute = "Wisdom is the lost property of a Muslim;\n hence collect it from whoever you find it with.";
+console.log("".concat(Auth, " once said, \"").concat(Qoute, "\""));
---
+++ Q4/index.ts
@@ -0,0 +1,5 @@
+let Auth: string = "Muhammad (S.A.W)";
+let Qoute: string = `Wisdom is the lost property of a Muslim;
+ hence collect it from whoever you find it with.`
+
+console.log(`${Auth} once said, "${Qoute}"`)
