487507e4a40a769fa3b1095ca4564e4dc9efc27c
Qalb-e-Abbas <sqabbas1994@gmail.com>
2023-08-02 23:39:10 +0500

    Q12 is committed & push on GitHub....

---
+++ Q12/index.ts
@@ -0,0 +1,7 @@
+let names : string [] = ["Asif","Aqib", "Waseem", "Usman", "Abdullah"];
+
+
+for (let f = 0; f < names.length; f++) {
+    
+    console.log(names[f], `I'm so grateful to have you in my life.`);
+}
