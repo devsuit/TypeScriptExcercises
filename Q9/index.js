c72451a1be51f848a33af67b24e5636e82d9fb4c
Qalb-e-Abbas <sqabbas1994@gmail.com>
2023-08-02 22:46:02 +0500

    Q9 is committed and Push on GitHub.

---
+++ Q9/index.js
@@ -0,0 +1,3 @@
+var favNum = 5;
+var msg = "My favorite Number is ".concat(favNum, ".");
+console.log(msg);
---
+++ Q9/index.ts
@@ -0,0 +1,6 @@
+let favNum : number = 5;
+
+let msg : string = `My favorite Number is ${favNum}.`;
+
+console.log(msg)
+
