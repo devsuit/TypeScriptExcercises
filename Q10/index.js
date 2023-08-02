af4a3003e1e0a0d534cd5d0661dea920398b0391
Qalb-e-Abbas <sqabbas1994@gmail.com>
2023-08-02 23:01:42 +0500

    Q10 is committed & push on GitHub

---
+++ Q10/index.js
@@ -0,0 +1,17 @@
+// Q9
+// Adding favorite number in a variable
+var favNum = 5;
+//Creating a message that reveals favorite number
+var msg = "My favorite Number is ".concat(favNum, ".");
+// Printing the message
+console.log(msg);
+"\n\n";
+//Q5
+// Adding famous person's name in a variable
+var famous_person = "Muhammad (S.A.W)";
+// Adding qoute in a variable
+var qoute = "Wisdom is the lost property of a Muslim;\n hence collect it from whoever you find it with.";
+//Creating a message for famous person's name and the quote
+var message = "".concat(famous_person, " once said, \"").concat(qoute, "\"");
+// Printing the message
+console.log(message);
---
+++ Q10/index.ts
@@ -0,0 +1,20 @@
+// Q9
+// Adding favorite number in a variable
+let favNum : number = 5;
+//Creating a message that reveals favorite number
+let msg : string = `My favorite Number is ${favNum}.`;
+// Printing the message
+console.log(msg);
+
+//----------------------------------------------------------------
+//Q5
+
+// Adding famous person's name in a variable
+let famous_person : string = "Muhammad (S.A.W)";
+// Adding qoute in a variable
+let qoute : string = `Wisdom is the lost property of a Muslim;
+ hence collect it from whoever you find it with.`;
+//Creating a message for famous person's name and the quote
+ let message: string = `${famous_person} once said, "${qoute}"`;
+// Printing the message
+console.log(message);    
