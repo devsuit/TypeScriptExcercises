7bbb68a9b900135d9435ac63cb753b9d4fe73792
Qalb-e-Abbas <sqabbas1994@gmail.com>
2023-08-02 23:19:04 +0500

    Q11 is committed & push on GitHub

---
+++ Q11/index.js
@@ -0,0 +1,4 @@
+var names = ["Asif", "Aqib", "Waseem", "Usman", "Abdullah"];
+for (var f = 0; f < names.length; f++) {
+    console.log(names[f]);
+}
---
+++ Q11/index.ts
@@ -0,0 +1,6 @@
+let names : string [] = ["Asif","Aqib", "Waseem", "Usman", "Abdullah"];
+
+
+for (let f = 0; f < names.length; f++) {
+    console.log(names[f]);
+}
