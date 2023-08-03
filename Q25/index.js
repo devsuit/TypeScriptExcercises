e1a6ae9ee4228fc666379448311218895135aa3e
Qalb-e-Abbas <sqabbas1994@gmail.com>
2023-08-04 00:25:18 +0500

    Q25 is committed and push on GitHub Repo..

---
+++ Q25/index.js
@@ -0,0 +1,10 @@
+// Passing version
+var alien_color_pass = 'green';
+if (alien_color_pass === 'green') {
+    console.log("Congratulations! You just earned 5 points.");
+}
+// Failing version
+var alien_color_fail = 'yellow';
+if (alien_color_fail === 'green') {
+    console.log("Congratulations! You just earned 5 points.");
+}
---
+++ Q25/index.ts
@@ -0,0 +1,13 @@
+// Passing version
+let alien_color_pass: string = 'green';
+
+if (alien_color_pass === 'green') {
+  console.log("Congratulations! You just earned 5 points.");
+}
+
+// Failing version
+let alien_color_fail: string = 'yellow';
+
+if (alien_color_fail === 'green') {
+  console.log("Congratulations! You just earned 5 points.");
+}
