70b10b5ef74ded7e28fea06e133f7005967a1b60
Qalb-e-Abbas <sqabbas1994@gmail.com>
2023-08-03 16:49:11 +0500

    Q13 is committed and push on Git hut Repo

---
+++ Q13/index.js
@@ -0,0 +1,5 @@
+var motorcycles = ["Honda", "Yamaha", "Kawasaki", "Suzuki"];
+console.log("I would like to own a ".concat(motorcycles[0], " motorcycle."));
+console.log("I would like to own a ".concat(motorcycles[1], " motorcycle."));
+console.log("I would like to own a ".concat(motorcycles[2], " motorcycle."));
+console.log("I would like to own a ".concat(motorcycles[3], " motorcycle."));
---
+++ Q13/index.ts
@@ -0,0 +1,6 @@
+let  motorcycles: string[] = ["Honda","Yamaha", "Kawasaki",  "Suzuki"];
+
+console.log(`I would like to own a ${motorcycles[0]} motorcycle.`);
+console.log(`I would like to own a ${motorcycles[1]} motorcycle.`);
+console.log(`I would like to own a ${motorcycles[2]} motorcycle.`);
+console.log(`I would like to own a ${motorcycles[3]} motorcycle.`);
