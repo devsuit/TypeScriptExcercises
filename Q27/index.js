af97988a519999d6ade0088d0b1ed5635ff197b9
Qalb-e-Abbas <sqabbas1994@gmail.com>
2023-08-04 00:36:10 +0500

    Q27 is committed and push on github.

---
+++ Q27/index.js
@@ -0,0 +1,24 @@
+// (Green Alien):
+var alien_color_v1 = 'green';
+if (alien_color_v1 === 'green') {
+    console.log("Version 1: Congratulations! You just earned 5 points for shooting the green alien.");
+}
+// (Yellow Alien):
+var alien_color_v2 = 'yellow';
+if (alien_color_v2 === 'green') {
+    console.log("Version 2: Congratulations! You just earned 5 points for shooting the green alien.");
+}
+else if (alien_color_v2 === 'yellow') {
+    console.log("Version 2: Congratulations! You just earned 10 points for shooting the yellow alien.");
+}
+//(Red Alien):
+var alien_color_v3 = 'red';
+if (alien_color_v3 === 'green') {
+    console.log("Version 3: Congratulations! You just earned 5 points for shooting the green alien.");
+}
+else if (alien_color_v3 === 'yellow') {
+    console.log("Version 3: Congratulations! You just earned 10 points for shooting the yellow alien.");
+}
+else if (alien_color_v3 === 'red') {
+    console.log("Version 3: Congratulations! You just earned 15 points for shooting the red alien.");
+}
---
+++ Q27/index.ts
@@ -0,0 +1,26 @@
+// (Green Alien):
+let alien_color_v1: string = 'green';
+
+if (alien_color_v1 === 'green') {
+  console.log("Version 1: Congratulations! You just earned 5 points for shooting the green alien.");
+}
+
+// (Yellow Alien):
+let alien_color_v2: string = 'yellow';
+
+if (alien_color_v2 === 'green') {
+  console.log("Version 2: Congratulations! You just earned 5 points for shooting the green alien.");
+} else if (alien_color_v2 === 'yellow') {
+  console.log("Version 2: Congratulations! You just earned 10 points for shooting the yellow alien.");
+}
+
+//(Red Alien):
+let alien_color_v3: string = 'red';
+
+if (alien_color_v3 === 'green') {
+  console.log("Version 3: Congratulations! You just earned 5 points for shooting the green alien.");
+} else if (alien_color_v3 === 'yellow') {
+  console.log("Version 3: Congratulations! You just earned 10 points for shooting the yellow alien.");
+} else if (alien_color_v3 === 'red') {
+  console.log("Version 3: Congratulations! You just earned 15 points for shooting the red alien.");
+}
