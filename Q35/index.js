4bc672cf6b9198cd41768f73bd432299f38d301e
Qalb-e-Abbas <sqabbas1994@gmail.com>
2023-08-04 15:30:41 +0500

    Q35 is committed & push on GitHUB Repo..

---
+++ Q35/index.js
@@ -0,0 +1,6 @@
+var common_animals = ["Dog", "Rabbit", "Cat"];
+for (var _i = 0, common_animals_1 = common_animals; _i < common_animals_1.length; _i++) {
+    var animal = common_animals_1[_i];
+    console.log("A ".concat(animal.toLowerCase(), " would make a great pet."));
+}
+console.log("Any of these animals would make a great pet!");
---
+++ Q35/index.ts
@@ -0,0 +1,7 @@
+let common_animals: string[] = ["Dog", "Rabbit", "Cat"];
+
+for (let animal of common_animals) {
+  console.log(`A ${animal.toLowerCase()} would make a great pet.`);
+}
+
+console.log("Any of these animals would make a great pet!");
