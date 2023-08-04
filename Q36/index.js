4252810ab9ec0d3270359dca173f89713730f786
Qalb-e-Abbas <sqabbas1994@gmail.com>
2023-08-04 16:04:30 +0500

    Q36 is committed and push on Github..

---
+++ Q36/index.js
@@ -0,0 +1,5 @@
+function make_shirt(size, message) {
+    console.log("You ordered a ".concat(size, " shirt with the message: \"").concat(message, "\""));
+}
+// Calling the function
+make_shirt("Medium", "I heart TypeScript!");
