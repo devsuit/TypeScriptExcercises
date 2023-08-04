4f2eb8c36d2da685396acc2cbb1da611da156138
Qalb-e-Abbas <sqabbas1994@gmail.com>
2023-08-04 14:43:59 +0500

    Q29 is committed & push on GitHub...

---
+++ Q29/index.js
@@ -0,0 +1,16 @@
+var favorite_fruits = ["bananas", "guava", "mangoes", "oranges", "grapes"];
+if (favorite_fruits.indexOf("bananas") !== -1) {
+    console.log("You really like bananas!");
+}
+if (favorite_fruits.indexOf("guava") !== -1) {
+    console.log("You really like guava!");
+}
+if (favorite_fruits.indexOf("mangoes") !== -1) {
+    console.log("You really like mangoes!");
+}
+if (favorite_fruits.indexOf("oranges") !== -1) {
+    console.log("You really like oranges!");
+}
+if (favorite_fruits.indexOf("grapes") !== -1) {
+    console.log("You really like grapes!");
+}
---
+++ Q29/index.ts
@@ -0,0 +1,21 @@
+let favorite_fruits: string[] = ["bananas", "guava", "mangoes","oranges","grapes"];
+
+if (favorite_fruits.indexOf("bananas") !== -1) {
+  console.log("You really like bananas!");
+}
+
+if (favorite_fruits.indexOf("guava") !== -1) {
+  console.log("You really like guava!");
+}
+
+if (favorite_fruits.indexOf("mangoes") !== -1) {
+  console.log("You really like mangoes!");
+}
+
+if (favorite_fruits.indexOf("oranges") !== -1) {
+  console.log("You really like oranges!");
+}
+
+if (favorite_fruits.indexOf("grapes") !== -1) {
+  console.log("You really like grapes!");
+}
