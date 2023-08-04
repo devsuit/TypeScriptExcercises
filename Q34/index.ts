7fbb7b58777ccf3e4a91ad03d86364186f1e7ad0
Qalb-e-Abbas <sqabbas1994@gmail.com>
2023-08-04 15:26:16 +0500

    Q34 is committed and push on github

---
+++ Q34/index.ts
@@ -0,0 +1,7 @@
+let favorite_pizzas: string[] = ["Cheese Pizza", "Veggie Pizza", "BBQ Chicken"];
+
+for (let pizza of favorite_pizzas) {
+  console.log(`I like ${pizza} pizza.`);
+}
+
+console.log("I really love pizza!");
