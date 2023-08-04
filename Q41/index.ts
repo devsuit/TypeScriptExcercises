78b9d931f7b4f74caa669d64ab8a6ddb4bd71f06
Qalb-e-Abbas <sqabbas1994@gmail.com>
2023-08-04 17:00:24 +0500

    Q41 is committed & push on github.

---
+++ Q41/index.ts
@@ -0,0 +1,10 @@
+function show_magicians(magicians: string[]): void {
+  for (let magician of magicians) {
+    console.log(magician);
+  }
+}
+
+let magicianNames: string[] = ["Dynamo", "Harry Houdini", "Penn Jillette", "David Blaine"];
+
+
+show_magicians(magicianNames);
